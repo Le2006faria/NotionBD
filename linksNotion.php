716d6ff7b4944adeb344df530dd2cf69108a4322
Le2006Faria <le2006faria@gmail.com>
2024-06-24 21:21:58 -0300

    first commit

---
+++ linksNotion.php
@@ -0,0 +1,48 @@
+<?php 
+
+echo "
+Calculadora de notas
+https://accessible-tenor-552.notion.site/Calculadora-de-notas-51dea76566314aab846df489475a8dbe
+";
+
+echo "
+Atividade CREATE- INSERT -DELETE - UPDATE
+https://accessible-tenor-552.notion.site/Atividade-CREATE-INSERT-DELETE-UPDATE-af569d7f80f4499d9324daa53b70d4dd
+";
+
+echo "
+BD - IMPORT / EXPORT - CREATE INDEX - DROP INDEX - DELETE
+https://accessible-tenor-552.notion.site/BD-IMPORT-EXPORT-CREATE-INDEX-DROP-INDEX-DELETE-dee9a51ac9744e42a5f78753adb5b0d2
+";
+
+echo "
+BD - INNER JOIN
+https://accessible-tenor-552.notion.site/BD-INNER-JOIN-e05acc15c50a4e8db4bd385ad7c661b3
+";
+
+echo "
+BD - INNER JOIN - AULA PRATICA
+https://accessible-tenor-552.notion.site/BD-INNER-JOIN-AULA-PRATICA-977e740f0aa24709b851f0d717c77daa
+";
+
+echo "
+# BD- Cláusulas GROUP BY - ORDER BY - OPERADORES ARITMETICOS, LÓGICOS e AGREGAÇÃO
+https://accessible-tenor-552.notion.site/BD-Cl-usulas-GROUP-BY-ORDER-BY-OPERADORES-ARITMETICOS-L-GICOS-e-AGREGA-O-7d2ab03a48554b10b6890cba29cd0971
+";
+
+echo "
+BD-LEFT JOIN - RIGHT JOIN  - FULL JOIN
+https://accessible-tenor-552.notion.site/BD-LEFT-JOIN-RIGHT-JOIN-FULL-JOIN-6e253cc026bd41818d5be28057a2f71a
+";
+
+echo "
+PROVA 2 - FAÇA NO PAPEL
+https://accessible-tenor-552.notion.site/PROVA-2-FA-A-NO-PAPEL-6a8cabf9eb4b4f71a643eab9578867b0
+";
+
+echo "
+BD-DCL- GRANT e REVOKE
+https://accessible-tenor-552.notion.site/BD-DCL-GRANT-e-REVOKE-ed12cf9a758c45698f1295367b589a1f
+";
+
+?>
